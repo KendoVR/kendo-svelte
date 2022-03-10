82ce77ee4633828611759d7797dadfb9c8bd9866
Yanko Dzhemerenov <idjemere@progress.com>
2022-03-10 12:17:47 +0200

    add @progress/kendo-theme-defaut

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,4 +1,5 @@
 import App from './App.svelte';
+import '@progress/kendo-theme-default';
 
 
 const app = new App({
--- src/Button/Button.svelte
+++ src/Button/Button.svelte
@@ -1,7 +1,7 @@
 <script>
-	export let name;
+	export let text;
 </script>
 
 <main>
-	<button>{name}</button>
+	<button type="button" class="k-button k-button-md k-rounded-md k-button-rectangle k-button-solid k-button-solid-base"><span class="k-button-text">{text}</span></button>
 </main>
